4ee0426c3af826a5adf45565060d0842eda96f48
Gena <gena@e64.net>
2017-03-27 23:12:04 +0200

    Combien de mots contiennent la lettre « w » ?

--- dico.php
+++ dico.php
@@ -5,17 +5,20 @@ $dico = explode("\n", $string);
 echo 'Combien de mots contient ce dictionnaire ? : ' . count($dico).'<br />';
 
 
+
 $nombreMotsTrouves = 0;
 $nombreMotsTrouvesAvecW = 0;
+$nombreMotsTrouvesAvecQ = 0;
 foreach ($dico as $key => $mot) {
 	$tailleMot = strlen($mot);
 	if($tailleMot===15){
 		$nombreMotsTrouves++;
 	}
 	$positionW = strpos($mot, 'w');
-	if($positionW >== 0){
+	if($positionW !== false){
 		$nombreMotsTrouvesAvecW++;
 	}
+
 }
 echo "Combien de mots font exactement 15 caractères ? : ".$nombreMotsTrouves."<br/>";
 echo "Combien de mots contiennent la lettre « w » ? : ".$nombreMotsTrouvesAvecW."<br/>";
