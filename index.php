3ef6848dd256cf17b4dd6bdfbc891a196dddb3fc
Gena <gena@e64.net>
2017-04-02 18:33:13 +0200

    Ajout d'une page d'accueil

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Php Crunching Géna</title>
+</head>
+<body>
+
+	<h1>Php Crunching</h1>
+	<ul>
+		<li><a href="dico.php">Exercices sur le dico</a></li>
+		<li><a href="film.php">Exercices sur les Films</a></li>
+	</ul>
+</body>
+</html>
